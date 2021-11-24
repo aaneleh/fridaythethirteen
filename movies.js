9494d166d7229092c36702929fecbb85e2a221df
aaneleh <helenakurzgon@gmail.com>
2021-11-24 14:38:04 -0300

    ranking section added

--- movies.js
+++ movies.js
@@ -1,26 +1,39 @@
 import './style.css'
 import * as data from '/data.json';
 
+//html element that will be edited
 const section = document.querySelector('#movies');
 
+//initiates the object to then add the data
 var movies = {
   titles: [],
   year: [],
   src: [],
   srcLocal: [],
   description: [],
-  rating: [ [ ] ]
+  avgRating: []
 }
-
+//Puts all the json information in a object to read it more easily
+var sumRating;
+var average;
 for(var i =0; i<data.movies.length; i++){
   movies.titles.push( data.movies[i].title  );
   movies.year.push  ( data.movies[i].year   );
   movies.src.push   ( data.movies[i].image  );
   movies.srcLocal.push   ( data.movies[i].imageLocal  );
   movies.description.push   ( data.movies[i].description  );
+  
+  sumRating = 0;
+  for(j=0;j< data.movies[i].rating.length; j++){
+    sumRating += data.movies[i].rating[j];
+  }
+  average = sumRating / data.movies[i].rating.length
+  movies.avgRating.push (Math.round(average * 100) / 100);
 }
-
-for(var j = 0; j< movies.titles.length; j++){
+/* MOVIES SECTIONS */
+//Writes all the films from the movies section with the information on the json
+//TRANSFORMAR ISSO NUMA FUNÇÃO QUE RETORNA TODA A DIV CRIADA DAÍ SÓ CHAMAR A FUNÇÃO E ApendiceChild NA SESSÃO 
+for(var j = 0; j < movies.titles.length; j++){
   if(j % 2 == 0){
     section.innerHTML+= `
       <div class ="movies-even">
@@ -81,9 +94,44 @@ for(var j = 0; j< movies.titles.length; j++){
         </form>
         
       </div>
-    </div>
-    `
+      </div>
+      `
+    }
   }
+
+/* RANKING SECTION */
+//Saves the order of the highests rankings
+var rankIndex = [ ];
+
+//copy all the ratings to another array so now I can change the numbers to find the info I need
+var avgRantings = [];
+for(i=0;i<movies.avgRating.length;i++){
+  avgRantings[i] = movies.avgRating[i];
 }
 
+//Code to find the highests ranking, I spent way to much time to do this, I think it could be better, but hey, it works
+//made it into a function to avoid ignoring equal numbers
+function seekHighest(array,number){
+  for(j = 0;j < array.length; j++){
+    if(array[j] == number){
+      return j;
+    }
+  }
+}
+var highestNumber;
+var highestNumberIndex;
+//find the highest number in the array then find its index using the function above then "deletes" this number and saves its position
+for(i = 0; i < 5; i++){
+  highestNumber       = Math.max.apply(null,avgRantings)
+  highestNumberIndex  = seekHighest(avgRantings,highestNumber);
+  rankIndex[i]        = highestNumberIndex;
+  avgRantings[highestNumberIndex] = null;
 
+}
+//writes the title of the top 5 movies in the html
+for(i = 0; i< 5;i++){
+  console.log(movies.avgRating[rankIndex[i]]);
+  document.querySelector('#rank-' + (i+1) +' h2').innerHTML = movies.titles[rankIndex[i]];
+  document.querySelector('#rank-' + (i+1) +' img').src = movies.srcLocal[rankIndex[i]];
+  document.querySelector('#rank-' + (i+1) +' h3').innerHTML = movies.avgRating[rankIndex[i]];
+}
